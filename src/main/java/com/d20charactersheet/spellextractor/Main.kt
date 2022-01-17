f2a8bfc86bbd150bd38523e5f60e4306a8358716
Torsten Wiederkehr <torsten.wiederkehr@gmail.com>
2022-01-17 06:27:13 +0100

    Implemented main to run whole parsing process

--- src/main/java/com/d20charactersheet/spellextractor/Main.kt
+++ src/main/java/com/d20charactersheet/spellextractor/Main.kt
@@ -1,5 +1,11 @@
 package com.d20charactersheet.spellextractor
 
 fun main() {
-    SpellExtractor().downloadSpells("src/main/resources/spell_names.txt")
+//    SpellExtractor().downloadSpells("src/main/resources/spell_names.txt")
+
+    val spellNames = SpellStorage().loadSpellNamesFromFile("src/main/resources/spell_names.txt")
+
+    val spellExtractor = SpellExtractor()
+    val spells = spellExtractor.parseSpells("src/main/resources/spells", spellNames)
+    spellExtractor.saveSpells("src/main/resources", spells)
 }
