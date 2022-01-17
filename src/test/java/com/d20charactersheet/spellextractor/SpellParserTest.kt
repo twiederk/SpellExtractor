c918c3fe78b49d5d87476b8cb68749dfa956a56d
Torsten Wiederkehr <torsten.wiederkehr@gmail.com>
2022-01-17 08:10:48 +0100

    Improved RegEx of description and replace double quote with single quote in description

--- src/test/java/com/d20charactersheet/spellextractor/SpellParserTest.kt
+++ src/test/java/com/d20charactersheet/spellextractor/SpellParserTest.kt
@@ -118,4 +118,33 @@ class SpellParserTest {
         )
     }
 
+    @Test
+    fun parseSpell_animalMessenger_spellData() {
+        // arrange
+        val spellName = "Animal Messenger"
+
+        val spellHtml =
+            """<div class="bloc"><h1>Animal Messenger</h1><div class="trad">
+            |<div><strong>Casting Time</strong>: 1 action</div>
+            |<div><strong>Range</strong>: 30 feet</div><div>
+            |<div><strong>Components</strong>: V, S, M (a morsel of food)</div>
+            |<div><strong>Duration</strong>: 24 hours</div>
+            |<div class="description ">By means of this spell, you use an animal to deliver a message. Choose a Tiny beast you can see within range, such as a squirrel, a blue jay, or a bat. You specify a location, which you must have visited, and a recipient who matches a general description, such as "a man or woman dressed in the uniform of the town guard" or "a red-haired dwarf wearing a pointed hat." You also speak a message of up to twenty-five words. The target beast travels for the duration of the spell toward the specified location, covering about 50 miles per 24 hours for a flying messenger, or 25 miles for other animals.<br />When the messenger arrives, it delivers your message to the creature that you described, replicating the sound of your voice. The messenger speaks only to a creature matching the description you gave. If the messenger doesn't reach its destination before the spell ends, the message is lost, and the beast makes its way back to where you cast this spell.<br /><strong><em>At Higher Levels</em></strong>. If you cast this spell using a spell slot of 3nd level or higher, the duration of the spell increases by 48 hours for each slot level above 2nd.<br /></div>""".trimMargin()
+                .trimIndent()
+
+
+        // act
+        val spell = SpellParser().parseSpell(spellName, spellHtml)
+
+        // assert
+        assertThat(spell.spellName).isEqualTo("Animal Messenger")
+        assertThat(spell.parsedSpellName).isEqualTo("Animal Messenger")
+        assertThat(spell.components).isEqualTo("V, S, M (a morsel of food)")
+        assertThat(spell.range).isEqualTo("30 feet")
+        assertThat(spell.duration).isEqualTo("24 hours")
+        assertThat(spell.description).isEqualTo(
+            "By means of this spell, you use an animal to deliver a message. Choose a Tiny beast you can see within range, such as a squirrel, a blue jay, or a bat. You specify a location, which you must have visited, and a recipient who matches a general description, such as 'a man or woman dressed in the uniform of the town guard' or 'a red-haired dwarf wearing a pointed hat.' You also speak a message of up to twenty-five words. The target beast travels for the duration of the spell toward the specified location, covering about 50 miles per 24 hours for a flying messenger, or 25 miles for other animals.<br />When the messenger arrives, it delivers your message to the creature that you described, replicating the sound of your voice. The messenger speaks only to a creature matching the description you gave. If the messenger doesn't reach its destination before the spell ends, the message is lost, and the beast makes its way back to where you cast this spell.<br /><strong><em>At Higher Levels</em></strong>. If you cast this spell using a spell slot of 3nd level or higher, the duration of the spell increases by 48 hours for each slot level above 2nd.<br />"
+        )
+    }
+
 }
