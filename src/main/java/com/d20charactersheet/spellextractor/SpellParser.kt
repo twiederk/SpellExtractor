c3561ef764952092f5b16dc79338ec75f2af0507
Torsten Wiederkehr <torsten.wiederkehr@gmail.com>
2022-01-17 08:02:01 +0100

    Replace ; with , and improved RegEx of range and description

--- src/main/java/com/d20charactersheet/spellextractor/SpellParser.kt
+++ src/main/java/com/d20charactersheet/spellextractor/SpellParser.kt
@@ -3,12 +3,12 @@ package com.d20charactersheet.spellextractor
 class SpellParser {
 
     fun parseSpell(spellName: String, spellHtml: String): Spell {
-        val cleanedSpellHtml = spellHtml.replace("—", "-")
+        val cleanedSpellHtml = spellHtml.replace("—", "-").replace(";", ",")
 
         val nameRegEx: Regex = """<h1>([A-Za-z/ ']+)</h1>""".toRegex()
         val parsedSpellName = nameRegEx.find(cleanedSpellHtml)?.groupValues?.get(1) ?: "error occurred while parsing name"
 
-        val rangeRegEx: Regex = """<strong>Range</strong>: ([0-9a-z ]+)</div>""".toRegex()
+        val rangeRegEx: Regex = """<strong>Range</strong>: ([A-Za-z0-9 ]+)</div>""".toRegex()
         val range = rangeRegEx.find(cleanedSpellHtml)?.groupValues?.get(1) ?: "error occurred while parsing range"
 
         val componentsRegEx: Regex = """<div><strong>Components</strong>: ([A-Za-z,() ']+)</div>""".toRegex()
@@ -17,7 +17,7 @@ class SpellParser {
         val durationRegEx: Regex = """<div><strong>Duration</strong>: ([A-Za-z0-9:, ]+)</div>""".toRegex()
         val duration = durationRegEx.find(cleanedSpellHtml)?.groupValues?.get(1) ?: "error occurred while parsing duration"
 
-        val descriptionRegEx: Regex = """<div class="description ">([A-Za-z0-9,.</>\-() '\n]+)</div>""".toRegex()
+        val descriptionRegEx: Regex = """<div class="description ">([A-Za-z0-9,.</>\-()+ '\n]+)</div>""".toRegex()
         val description = descriptionRegEx.find(cleanedSpellHtml)?.groupValues?.get(1) ?: "error occurred while parsing description"
 
         return Spell(
--- src/test/java/com/d20charactersheet/spellextractor/SpellParserTest.kt
+++ src/test/java/com/d20charactersheet/spellextractor/SpellParserTest.kt
@@ -89,4 +89,33 @@ class SpellParserTest {
         )
     }
 
+    @Test
+    fun parseSpell_alterSelfArrow_spellData() {
+        // arrange
+        val spellName = "Alter Self"
+
+        val spellHtml =
+            """<div class="bloc"><h1>Alter Self</h1><div class="trad">
+            |<div><strong>Casting Time</strong>: 1 action</div>
+            |<div><strong>Range</strong>: Self</div>
+            |<div><strong>Components</strong>: V, S</div>
+            |<div><strong>Duration</strong>: Concentration, up to 1 hour</div>
+            |<div class="description ">You assume a different form. When you cast the spell, choose one of the following options, the effects of which last for the duration of the spell. While the spell lasts, you can end one option as an action to gain the benefits of a different one.<br /><strong><em>Aquatic Adaptation</em></strong>. You adapt your body to an aquatic environment, sprouting gills and growing webbing between your fingers. You can breathe underwater and gain a swimming speed equal to your walking speed.<br /><strong><em>Change Appearance</em></strong>. You transform your appearance. You decide what you look like, including your height, weight, facial features, sound of your voice, hair length, coloration, and distinguishing characteristics, if any. You can make yourself appear as a member of another race, though none of your statistics change. You also can't appear as a creature of a different size than you, and your basic shape stays the same; if you're bipedal, you can't use this spell to become quadrupedal, for instance. At any time for the duration of the spell, you can use your action to change your appearance in this way again.<br /><strong><em>Natural Weapons</em></strong>. You grow claws, fangs, spines, horns, or a different natural weapon of your choice. Your unarmed strikes deal 1d6 bludgeoning, piercing, or slashing damage, as appropriate to the natural weapon you chose, and you are proficient with your unarmed strikes. Finally, the natural weapon is magic and you have a +1 bonus to the attack and damage rolls you make using it.<br /></div>""".trimMargin()
+                .trimIndent()
+
+
+        // act
+        val spell = SpellParser().parseSpell(spellName, spellHtml)
+
+        // assert
+        assertThat(spell.spellName).isEqualTo("Alter Self")
+        assertThat(spell.parsedSpellName).isEqualTo("Alter Self")
+        assertThat(spell.components).isEqualTo("V, S")
+        assertThat(spell.range).isEqualTo("Self")
+        assertThat(spell.duration).isEqualTo("Concentration, up to 1 hour")
+        assertThat(spell.description).isEqualTo(
+            "You assume a different form. When you cast the spell, choose one of the following options, the effects of which last for the duration of the spell. While the spell lasts, you can end one option as an action to gain the benefits of a different one.<br /><strong><em>Aquatic Adaptation</em></strong>. You adapt your body to an aquatic environment, sprouting gills and growing webbing between your fingers. You can breathe underwater and gain a swimming speed equal to your walking speed.<br /><strong><em>Change Appearance</em></strong>. You transform your appearance. You decide what you look like, including your height, weight, facial features, sound of your voice, hair length, coloration, and distinguishing characteristics, if any. You can make yourself appear as a member of another race, though none of your statistics change. You also can't appear as a creature of a different size than you, and your basic shape stays the same, if you're bipedal, you can't use this spell to become quadrupedal, for instance. At any time for the duration of the spell, you can use your action to change your appearance in this way again.<br /><strong><em>Natural Weapons</em></strong>. You grow claws, fangs, spines, horns, or a different natural weapon of your choice. Your unarmed strikes deal 1d6 bludgeoning, piercing, or slashing damage, as appropriate to the natural weapon you chose, and you are proficient with your unarmed strikes. Finally, the natural weapon is magic and you have a +1 bonus to the attack and damage rolls you make using it.<br />"
+        )
+    }
+
 }
